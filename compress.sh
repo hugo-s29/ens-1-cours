5186bb411f8824f9737dac9f2aad434855f046e6
Hugo Salou <y.hugo.s29@gmail.com>
2025-03-10 23:47:22 +0100

    Compressing PDFs, but only if needed

--- compress.sh
+++ compress.sh
@@ -1,4 +1,8 @@
 # Compress ColaNote PDFs (because they're WAY TOO HEAVY)
+#
+# Should NEVER be ran directly, only copy the required line
+# and run it in a shell
+# otherwise it'll create endless versions of PDFs...
 
 convert -density 125 log/dm1-uncompressed.pdf -quality 99 -compress Zip log/dm1.pdf
 convert -density 125 proba/td-uncompressed.pdf -quality 99 -compress Zip proba/td.pdf
