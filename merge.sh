6555860942c9b6334edd954437e128924d29eb1b
Hugo Salou <y.hugo.s29@gmail.com>
2025-03-12 12:07:59 +0100

    Script to merge ALL PDFs

---
+++ merge.sh
@@ -0,0 +1,12 @@
+pdftk \
+  algebre1/fiche.pdf algebre1/cours/main.pdf algebre1/td/main.pdf algebre1/dm1/main.pdf algebre1/dm2/main.pdf \
+  algo1/main.pdf \
+  algo2/main.pdf algo2/dm1/main.pdf \
+  category-theory/main.pdf category-theory/dm1/main.pdf category-theory/dm2/main.pdf category-theory/dm3/main.pdf \
+  fdi/main.pdf fdi/dm1/main.pdf fdi/dm2/main.pdf fdi/dm3/main.pdf fdi/dm4/main.pdf fdi/dm5/main.pdf fdi/td8/main.pdf \
+  integration/main.pdf integration/dm2/main.pdf \
+  log/main.pdf log/td.pdf log/dm1.pdf \
+  proba/main.pdf proba/td.pdf \
+  profon/main.pdf profon/small-docs/trad.pdf profon/small-docs/opti.pdf \
+  thprog/main.pdf \
+cat output all.pdf
