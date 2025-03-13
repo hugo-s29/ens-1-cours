36cdf1b8f5de0efbe74e94f73ec5ed292c88e4ac
Hugo Salou <y.hugo.s29@gmail.com>
2025-03-13 16:42:36 +0100

    Proba[DM1]

--- compress.sh
+++ compress.sh
@@ -6,4 +6,5 @@
 
 convert -density 125 log/dm1-uncompressed.pdf -quality 99 -compress Zip log/dm1.pdf
 convert -density 125 log/td-uncompressed.pdf -quality 99 -compress Zip log/td.pdf
+convert -density 125 proba/dm1-uncompressed.pdf -quality 99 -compress Zip proba/dm1.pdf
 convert -density 125 proba/td-uncompressed.pdf -quality 99 -compress Zip proba/td.pdf
